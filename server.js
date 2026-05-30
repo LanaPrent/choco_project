2889e475f0c14d2a78f5153139faa23b7edc1764
Lana <sp.englishlang@yahoo.com>
2026-05-31 01:20:32 +0200

    Fix Railway deployment dependencies and server binding

--- server.js
+++ server.js
@@ -115,8 +115,8 @@ app.get("/", (req, res) => {
 // START SERVER
 // =======================
 
-app.listen(PORT, () => {
-
+//app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(
         `Server running on http://localhost:${PORT}`
     );
