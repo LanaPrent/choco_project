6c5986c241c9411429d8226bde43f41d56069684
Lana <sp.englishlang@yahoo.com>
2026-05-31 14:09:35 +0200

    Changed PORT number

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const isAuthenticated = require("./middleware/authMiddleware");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // =======================
 // STATIC FILES
